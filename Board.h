2df7a04fc030cb32a7c2d515e4792054193a5895
ausrinuxas <ausrine.ara@gmail.com>
2016-06-06 22:58:02 +0300

    Klases. mokymasis.

---
+++ Board.h
@@ -0,0 +1,17 @@
+#ifndef Board_h
+#define Board_h
+
+class Board
+{
+public:
+  Board();
+  ~Board();
+
+private:
+  vector<string> place;
+  int width, height;
+};
+
+
+
+#endif
--- main.cpp
+++ main.cpp
@@ -4,41 +4,41 @@
 #include <vector>
 #include <algorithm>
 
+#include <Board.h>
+
 using namespace std;
 
 int main()
 {
     string line;
-
-    vector<string> map; //[ ]
+    vector<string> map;
 
     int col(string in);
-    int row=0, colm=-1;
+    int row=0, column=-1;
     ifstream myfile ("map.txt");
     if (myfile.is_open())
     {
-      while ( getline (myfile,line) )
+    while ( getline (myfile,line) )
+    {
+      if(column == -1)
       {
-        if(colm == -1)
+        column = 0;
+        for (unsigned int i = 0;i < line.size(); i++)
         {
-          colm = 0;
-          for (unsigned int i = 0;i < line.size(); i++)
-          {
-            if (line[i] != ' ')
-            {
-              ++colm;
-            }
-          }
+          if (line[i] != ' ')
+          ++column;
         }
-        map.push_back(line);
-        cout << line << '\n';
-        ++row;
-
+      }
+      map.push_back(line);
+      cout << line << '\n';
+      ++row;
       }
       myfile.close();
-    }
+      }
 
     else cout << "Unable to open file";
-    cout << "eilutes: " << row << "   stulpeliai: " << colm << endl;
+
+    cout << "eilutes: " << row << "   stulpeliai: " << column << endl;
+
    return 0;
 }
